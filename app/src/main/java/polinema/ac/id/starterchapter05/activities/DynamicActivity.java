3724e1cb8bd10d20b23785cfdb7fd6989f4b0a51
LyneD <krisna.widi.kw@gmail.com>
2019-09-29 19:12:16 +0700

    Transition Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -20,6 +20,7 @@ public class DynamicActivity extends AppCompatActivity {
 
     public void handlerClickLoadRedFragment(View view) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
@@ -29,6 +30,7 @@ public class DynamicActivity extends AppCompatActivity {
 
     public void handleClickLoadBlueFragment(View view) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
