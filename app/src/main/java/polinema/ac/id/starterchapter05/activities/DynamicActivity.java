229e9f12beb339edf7bb35b38c29a2504aaeb10f
krisnawid <krisna.widi.kw@gmail.com>
2019-10-26 18:30:17 +0700

    Tugas Fragment Selesai

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -1,6 +1,7 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -12,6 +13,9 @@ import polinema.ac.id.starterchapter05.fragments.RedFragment;
 
 public class DynamicActivity extends AppCompatActivity {
 
+    public static final String RED_TAG = "RED_FRAGMENT";
+    public static final String BLUE_TAG = "BLUE_FRAGMENT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +23,27 @@ public class DynamicActivity extends AppCompatActivity {
     }
 
     public void handlerClickLoadRedFragment(View view) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.dynamic_fragment_placeholder);
 
-//        getSupportFragmentManager().beginTransaction().replace(R.id.dynamic_fragment_placeholder, new RedFragment()).commit();
+        if (fragment == null || fragment instanceof BlueFragment) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_right)
+                    .replace(R.id.dynamic_fragment_placeholder, new RedFragment(), RED_TAG)
+                    .addToBackStack(null)
+                    .commit();
+        }
+
     }
 
     public void handleClickLoadBlueFragment(View view) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment());
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.dynamic_fragment_placeholder);
+
+        if (fragment == null || fragment instanceof RedFragment) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_left, R.anim.exit_from_left, R.anim.enter_from_left, R.anim.exit_from_right)
+                    .replace(R.id.dynamic_fragment_placeholder, new BlueFragment(), BLUE_TAG)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/Handstand.java
@@ -0,0 +1,16 @@
+package polinema.ac.id.starterchapter05.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class Handstand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handstand);
+    }
+}
